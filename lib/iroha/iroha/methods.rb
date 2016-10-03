7f71804816041bb825e44944fde08f8d0bcb1db7
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2016-10-03 15:44:14 +0900

    [fix] Iroha.franchise_class method, to create franchise classes in Iroha::Resource::* and Iroha::Type::*

--- lib/iroha/iroha/methods.rb
+++ lib/iroha/iroha/methods.rb
@@ -8,32 +8,47 @@ module Iroha
     return parent_class
   end
 
+  DEBUG = false
+
   def self.franchise_class(franchisee, franchisor)
 
+    def self.p_info(message); p "== " + message if DEBUG == true; end
+
+    p_info("#{self}.franchise_class(#{franchisee}, #{franchisor}) start")
+
     Iroha::BASE_CLASS_NAME_LIST.each do |class_name|
+      p_info("  class  #{franchisee}::#{class_name} < #{franchisor.const_get(class_name)}")
       franchisee.const_set(class_name, Class.new(franchisor.const_get(class_name)))
+      p_info("  end")
     end
 
+    p_info("  module #{franchisee}::Resource")
     franchisee.const_set(:Resource, Module.new)
+    p_info("  end")
+
+    p_info("  module #{franchisee}::Type")
     franchisee.const_set(:Type    , Module.new)
+    p_info("  end")
 
-    franchisor_resource_class = franchisor.const_get(:IResource)
-    ObjectSpace.each_object(Class).select{|klass| klass.superclass == franchisor_resource_class}.each do |res_class|
+    franchisor_resource_module = franchisor.const_get(:Resource)
+    franchisor_resource_list   = franchisor_resource_module.constants.map{|c| franchisor_resource_module.const_get(c)}
+    franchisor_resource_list.each do |res_class|
       class_name = res_class.to_s.split(/::/).last.to_sym
-      if class_name != :IResource then
-        franchisee.const_get(:Resource).const_set(class_name, Class.new(res_class))
-        # p "== #{class_name} == #{franchisee.const_get(:Resource).const_get(class_name)}"
-      end
+      p_info("  class  #{franchisee.const_get(:Resource)}::#{class_name} < #{res_class}")
+      franchisee.const_get(:Resource).const_set(class_name, Class.new(res_class))
+      p_info("  end")
     end
 
-    franchisor_type_class = franchisor.const_get(:IType)
-    ObjectSpace.each_object(Class).select{|klass| klass.superclass == franchisor_type_class}.each do |type_class|
+    franchisor_type_module = franchisor.const_get(:Type)
+    franchisor_type_list   = franchisor_type_module.constants.map{|c| franchisor_type_module.const_get(c)}
+    franchisor_type_list.each do |type_class|
       class_name = type_class.to_s.split(/::/).last.to_sym
-      if class_name != :IType then
-        franchisee.const_get(:Type).const_set(class_name, Class.new(type_class))
-      end
+      p_info("  class #{franchisee.const_get(:Type)}::#{class_name} < #{type_class}")
+      franchisee.const_get(:Type).const_set(class_name, Class.new(type_class))
+      p_info("  end")
     end
 
+    p_info("#{self}.franchise_class(#{franchisee}, #{franchisor}) done")
   end
 
   def self.include_module(class_object, module_object)
