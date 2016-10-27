dad18c523b72f3794a86a5488b3733b557be254d
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2016-10-27 13:17:33 +0900

    [change] to simple using Wire assign ./example/div.rb and [add] test bench for ./example/div.iroha

---
+++ test/div/src/make_test_snr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void test(int i, uint32_t dividend, uint32_t divisor, uint32_t quotient, uint32_t remainder)
+{
+  printf("---\n");
+  printf("- I  : [{SAY: \"Test 1.%d : 0x%08X / 0x%08X => 0x%08X, 0x%08X (%d / %d => %d, %d %% %d => %d)\"}]\n", i, dividend, divisor, quotient, remainder, dividend, divisor, quotient, dividend, divisor, remainder);
+  printf("- I  : [{XFER: {DATA: [0x%08X, 0x%08X]}}]\n", dividend, divisor  );
+  printf("- O  : [{XFER: {DATA: [0x%08X, 0x%08X]}}]\n", quotient, remainder);
+}
+
+int main(argc, argv)
+{
+  int      i;
+  uint32_t dividend;
+  uint32_t divisor;
+  uint32_t quotient;
+  uint32_t remainder;
+  int      max_snr = 1000;
+
+  printf("---\n");
+  printf("- MARCHAL  : \n");
+  printf("  - SAY    : Fmul TEST 1 Start.\n");
+  
+  i = 1;
+  dividend  = 1;
+  divisor   = 1;
+  quotient  = 1;
+  remainder = 0;
+  test(i, dividend, divisor, quotient, remainder);
+  i++;
+  while (i <= max_snr) {
+    dividend  = (((uint32_t)rand() << 16) & 0x7FFF0000) | ((uint32_t)rand() & 0x0000FFFF);
+    divisor   = (((uint32_t)rand()   + 1) & 0x0000FFFF);
+    quotient  = dividend / divisor;
+    remainder = dividend % divisor;
+    test(i, dividend, divisor, quotient, remainder);
+    i++;
+  }
+  printf("---\n");
+  printf("- MARCHAL  : \n");
+  printf("  - SAY    : Fmul TEST 1 Done.\n");
+  printf("---\n");
+}
